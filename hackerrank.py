a3872c6ec092cc98e682c943ece4a9beea9651b5
MichaelDeme <micted94@gmail.com>
2022-04-05 19:08:05 +0200

    apr 5

--- hackerrank.py
+++ hackerrank.py
@@ -166,4 +166,12 @@ if __name__ == '__main__':
             print l
    
 
+#tuples
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    tup = tuple(integer_list)
+    print(hash(tup))
+   
       
