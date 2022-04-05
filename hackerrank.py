3ece19371d03e396b0752004005ea0d52f68f523
MichaelDeme <micted94@gmail.com>
2022-04-05 17:55:02 +0200

    init

--- hackerrank.py
+++ hackerrank.py
@@ -122,7 +122,25 @@ if __name__ == '__main__':
         print(el)
     
     
-            
+#finding percentage
+
+from decimal import Decimal
+if __name__ == '__main__':
+    n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
+    
+    find = student_marks[query_name]
+    
+    tot = sum(find)
+    avgd = Decimal(tot/3)
+
+    print("{:.2f}".format(avgd))    
                 
                 
         
